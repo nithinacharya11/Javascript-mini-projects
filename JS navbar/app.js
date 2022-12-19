dc82da67e84f90b91de604c7216d476f59d9054f
nithinacharya11 <nithinpacharya@gmail.com>
2022-12-19 13:29:27 +0530

    final commit

--- JS navbar/app.js
+++ JS navbar/app.js
@@ -2,6 +2,5 @@ const btn  = document.querySelector('.nav-toggle');
 const list = document.querySelector('.links');
 
 btn.addEventListener('click',function(){
-    
     list.classList.toggle('show-links')
 })
